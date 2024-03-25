18d7b4a931a857a076b67b5619636fed85c1a68b
GCAKuma <charithashen592@gmail.com>
2024-03-25 10:47:08 +0530

    porpertices

--- 24414/Employee.cs
+++ 24414/Employee.cs
@@ -6,9 +6,16 @@
         public string LastName { get; set; }
         public double BasicSalary { get; set; }
 
+        public Employee(string firstName, string lastName, double basicSalary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BasicSalary = basicSalary;
+        }
+
         public void show()
         {
-            Console.WriteLine("Name: " + FirstName + LastName + " Salary: " + BasicSalary);
+            Console.WriteLine("Name: " + FirstName+ " " + LastName + ", Salary: " + BasicSalary);
         }
     }
 }
--- 24414/Program.cs
+++ 24414/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("24414");
+            Employee em1 = new Employee("Charit","Ashen",85000);
+            Employee em2 = new Employee("Pramindu", "Vishwa", 82500);
+            Employee em3 = new Employee("Pradeem", "Madhushan", 78200);
+
+            em1.show();
+
+
         }
     }
 }
