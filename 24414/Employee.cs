b229924537d4e742ee008dd235b5515b2c320358
GCAKuma <charithashen592@gmail.com>
2024-03-25 10:27:50 +0530

    commit the class

---
+++ 24414/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace _24414
+{
+    internal class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double BasicSalary { get; set; }
+
+        public void show()
+        {
+            Console.WriteLine("Name: " + FirstName + LastName + " Salary: " + BasicSalary);
+        }
+    }
+}
