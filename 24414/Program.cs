b82d3a5ef85c128f10c70b096dfe79cf0a72512d
GCAKuma <charithashen592@gmail.com>
2024-03-25 10:16:12 +0530

    commit the solution

--- 24414/Program.cs
+++ 24414/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            //24414 charith;
+            int[] numbers = { 50, 78, 80, 10, 15, 13, 14, 12, 47, 85 };
+
+            double total = 0;
+
+            foreach(int i in numbers)
+            {
+                total += i;
+            }
+            double average=total / numbers.Length;
+            Console.WriteLine("Total = " + total + " , " + "Average = " + average);
+            
+
         }
     }
 }
